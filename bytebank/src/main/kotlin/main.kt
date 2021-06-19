6a0e8109b270f2ef7c0d3d5e3c119281eee06094
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2021-06-19 15:40:07 -0300

    tratando exceptions

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,4 +1,7 @@
 import br.com.alura.modelo.*
+import java.lang.ArithmeticException
+import java.lang.ClassCastException
+import java.lang.NumberFormatException
 import java.util.*
 
 // Ctrl + Alt + l : arrumar identação
@@ -8,7 +11,40 @@ var totalContas : Int = 0
 fun main(){
 
     println("Início main ")
-    funcao1()
+    val entrada: String = "1,9"
+    var valor: Double = 0.0
+    try {
+        valor = entrada.toDouble()
+    }catch (e:NumberFormatException){
+        println("Problema na conversão...")
+        // java.lang.NumberFormatException: For input string: "1,9"
+        //	at java.base/jdk.internal.math.FloatingDecimal.readJavaFormatString(FloatingDecimal.java:2054)
+        //	at java.base/jdk.internal.math.FloatingDecimal.parseDouble(FloatingDecimal.java:110)
+        //	at java.base/java.lang.Double.parseDouble(Double.java:543)
+        //	at MainKt.main(main.kt:17)
+        //	at MainKt.main(main.kt)
+        e.printStackTrace()
+    }
+
+
+    val valorComTaxa: Double? = if(valor != null){
+        valor + 0.1
+    }else{
+        null
+    }
+
+    val valorComTaxa2: Double? = when {
+        valor != null -> {
+            valor + 0.1
+        }
+        else -> {
+            null
+        }
+    }
+
+
+
+   // funcao1()
     println("Fim main ")
 
 }
@@ -30,6 +66,41 @@ fun funcao3(){
     println("Início funcao 3 ")
 
     for(i in 1..5){
+
+        // Exception in thread "main" java.lang.ClassCastException: class java.lang.Object cannot be cast to class br.com.alura.modelo.Endereco
+        // (java.lang.Object is in module java.base of loader 'bootstrap'; br.com.alura.modelo.Endereco is in unnamed module of loader 'app')
+        // tentando converter a classe de um tipo não compatível
+        //val endereco = Any()
+        //endereco as Endereco
+
+        try{
+            val endereco = Any()
+            endereco as Endereco
+        }catch (e: ClassCastException){
+            println("stack: " + e.stackTrace.toString())
+            println("message: " + e.message)
+            println("cause: " + e.cause)
+            println("CLASS CAST EXCEPTION")
+        }
+
+        // Exception in thread "main" java.lang.ArithmeticException: / by zero
+        //	at MainKt.funcao3(main.kt:39)
+        //	at MainKt.funcao2(main.kt:24)
+        //	at MainKt.funcao1(main.kt:18)
+        //	at MainKt.main(main.kt:11)
+        //	at MainKt.main(main.kt)
+        // var x = 10
+        // println(x/0)
+
+        try{
+            10/0
+        }catch (e: ArithmeticException){
+            println("stack: " + e.stackTrace.toString())
+            println("message: " + e.message)
+            println("cause: " + e.cause)
+            println("ARITHMETIC EXCEPTION")
+        }
+
         println(i)
     }
     println("Fim funcao 3 ")
